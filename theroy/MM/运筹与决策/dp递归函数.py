9ca6c668d8d49d74b850da23663570d3943fbc9f
presentjjjjk <3433138241@qq.com>
2024-07-29 16:56:41 +0800

    Deployed afc53a1 with MkDocs version: 1.6.0

--- theroy/MM/运筹与决策/dp递归函数.py
+++ theroy/MM/运筹与决策/dp递归函数.py
@@ -9,7 +9,7 @@ value=[10,40,30,50]
 
 #定义最优解数组
 
-t=[]
+t=[0 for _ in range(4)]
 
 def V(k,w):
     if k==0:
@@ -17,20 +17,23 @@ def V(k,w):
     else:
         if weight[k-1] <= w:
             if V(k-1,w)>V(k-1,w-weight[k-1])+value[k-1]:
-                t.insert(0,0)
-               
+                #t.insert(0,0)
+                #在比较大小的时候就会重复调用这个t的添加操作,所以不能这么做
+                #解决办法是定义一个数组,每次的重复调用函数会得到相同的操作,这样就行了.
+                t[k-1]=0
                 return V(k-1,w)
             else:
-                t.insert(0,1)
-                print(t)
+                t[k-1]=1
+        
                 return V(k-1,w-weight[k-1])+value[k-1]
             
 
         else:
-            t.insert(0,0)
+            t[k-1]=0
             return V(k-1,w)
         
 a=V(4,10)
 
 print(a)
 print(t)
+
