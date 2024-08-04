775ae49353e2e7766edd570d06cf07433daea560
presentjjjjk <3433138241@qq.com>
2024-08-04 23:14:57 +0800

    更新

--- docs/assets/javascripts/mathjax.js
+++ docs/assets/javascripts/mathjax.js
@@ -8,9 +8,13 @@ window.MathJax = {
   options: {
     ignoreHtmlClass: ".*|",
     processHtmlClass: "arithmatex"
+  },
+  chtml: {
+    scale: 0.85  // 将公式缩小到原来的 80%
   }
 };
 
 document$.subscribe(() => {
   MathJax.typesetPromise()
 })
+
