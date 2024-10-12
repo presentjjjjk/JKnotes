2bb7c4c9a6f0b87c055963da2402ef96eef3d8e1
presentjjjjk <3433138241@qq.com>
2024-10-12 14:41:06 +0800

    Deployed 47637f81 with MkDocs version: 1.6.1

--- CS/Python/面向对象/三大特性.py
+++ CS/Python/面向对象/三大特性.py
@@ -1,10 +1,43 @@
-class Mammal:
-    def eat(self):
-        print('老祖宗在吃东西')
+# class Mammal:
+#     def eat(self):
+#         print('老祖宗在吃东西')
 
-class Person(Mammal):
-    def eat(self):
-        print('人在吃东西')
+# class Person(Mammal):
+#     def eat(self):
+#         print('人在吃东西')
 
-person1=Person()
-person1.eat()
+# person1=Person()
+# person1.eat()
+
+# class Example:
+#     def __init__(self):
+#         self.__private_attr = 10  # 私有属性
+    
+#     def __private_method(self):  # 私有方法
+#         print("这是一个私有方法")
+    
+#     def public_method(self):
+#         print(self.__private_attr)
+#         self.__private_method()
+
+# example = Example()
+# example.public_method()
+
+class Example:
+    def __init__(self):
+        self.__private_attr = 10  # 私有属性
+    
+    def __private_method(self):  # 私有方法
+        return "这是一个私有方法的返回值"
+    
+    def public_method(self):
+        private_attr_value = self.__private_attr
+        private_method_result = self.__private_method()
+        return private_attr_value, private_method_result
+
+# 使用示例
+e = Example()
+attr_value, method_result = e.public_method()
+
+print(f"私有属性的值: {attr_value}")
+print(f"私有方法的返回值: {method_result}")
